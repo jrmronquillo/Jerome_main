2db50f4928c720a21bbd6735686bc154b62ae5c2
System Administrator <root@ELSML03JR909A-4.local>
2018-07-24 11:33:43 -0700

    added script line to allow for JSX react implementation, changed react template to include two examples

--- static/like_button.js
+++ static/like_button.js
@@ -1,20 +1,72 @@
 'use strict';
 
-const Items = (props) => {
-  return <h1>Current Quantity of Items in Cart: {props.quantity}</h1>
-}
 
-Items.defaultProps = {
-  quantity: 0
-}
-
-class ShoppingCart extends React.Component {
+class MyComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      itemCount: 0
+    };
+    // change code below this line
+    this.addItem = this.addItem.bind(this)
+    // change code above this line
+  }
+  addItem() {
+    this.setState({
+      itemCount: this.state.itemCount + 1
+    });
   }
   render() {
-    { /* change code below this line */ }
-    return <Items quantity={10}/>
-    { /* change code above this line */ }
+    return (
+      <div>
+        { /* change code below this line */ }
+        <button onClick={this.addItem}>Click Me</button>
+        { /* change code above this line */ }
+        <h1>Current Item Count: {this.state.itemCount}</h1>
+      </div>
+    );
   }
 };
+
+
+
+class MyComponent2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+    // change code below this line
+    this.toggleVisibility = this.toggleVisibility.bind(this)
+    // change code above this line
+  }
+  // change code below this line
+  toggleVisibility(){
+    this.setState({
+      visibility: !this.state.visibility
+    })
+  }
+  // change code above this line
+  render() {
+    if (this.state.visibility) {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+          <h1>Now you see me!</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <button onClick={this.toggleVisibility}>Click Me</button>
+        </div>
+      );
+    }
+  }
+};
+
+const domContainer = document.querySelector('#react_container');
+ReactDOM.render(< MyComponent />, document.getElementById('react_container'));
+
+const domContainer2 = document.querySelector('#react_container2');
+ReactDOM.render(< MyComponent2 />, document.getElementById('react_container2'));
