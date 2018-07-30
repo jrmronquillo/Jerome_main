766ab9e45acaae31bca8abb95caee2679d7df62c
RONQUILLO <jr9099@ELSML03JR909A-4.local>
2018-07-30 16:18:39 -0700

    add modals for the controller-app-example

--- main.py
+++ main.py
@@ -26,12 +26,19 @@ def reactPlayground():
 
 @app.route('/controller-app-example', methods=['GET', 'POST'])
 @app.route('/controller-app-example/', methods=['GET', 'POST'])
-@app.route('/controller-app-example/<string:button_set>/', methods=['GET', 'POST'])
-@app.route('/controller/<string:button_set>/<string:quad>/', methods=['GET', 'POST'])
-@app.route('/controller/<string:button_set>/<string:quad>/<int:rack_id>/', methods=['GET', 'POST'])
-@app.route('/controller/<string:button_set>/<string:quad>/<int:rack_id>/<string:slot_id>/', methods=['GET', 'POST'])
-@app.route('/controller/<string:button_set>/<string:quad>/<int:rack_id>/<string:slot_id>/', methods=['GET', 'POST'])
-def controller_app_example(button_set="main", rack_id="0", slot_id="0", quad='noQuad'):
+@app.route('/controller-app-example/<string:modalMode>', methods=['GET', 'POST'])
+@app.route('/controller-app-example/<string:modalMode>/<string:button_set>/', methods=['GET', 'POST'])
+@app.route('/controller/<string:modalMode>/<string:button_set>/<string:quad>/', methods=['GET', 'POST'])
+@app.route('/controller/<string:modalMode>/<string:button_set>/<string:quad>/<int:rack_id>/', methods=['GET', 'POST'])
+@app.route('/controller/<string:modalMode>/<string:button_set>/<string:quad>/<int:rack_id>/<string:slot_id>/', methods=['GET', 'POST'])
+@app.route('/controller/<string:modalMode>/<string:button_set>/<string:quad>/<int:rack_id>/<string:slot_id>/', methods=['GET', 'POST'])
+@app.route('/controller/<string:modalMode>/<string:button_set>/<string:quad>/<int:rack_id>/<string:slot_id>/', methods=['GET', 'POST'])
+
+def controller_app_example(modalMode='false',button_set="main", rack_id="0", slot_id="0", quad='noQuad'):
+    if request.method == 'POST':
+        print 'post detected!!!!'
+        return render_template('controller-app-example.html', modalMode='rightside', rack_id=rack_id, slot_id=slot_id, button_set=button_set, quad=quad)
+
     # if not rack_id:
     #    return "rack_id was undefined"
     print "button_set:"
@@ -77,128 +84,15 @@ def controller_app_example(button_set="main", rack_id="0", slot_id="0", quad='no
         if quad != "true":
             print "No valid Rack Selected"
             flash("Please select Rack" )
-            return render_template('controller-app-example.html', button_set=button_set, quad=quad)
-
+            return render_template('controller-app-example.html', button_set=button_set, quad=quad, modalMode=modalMode)
+    else:
+        return render_template('controller-app-example.html', button_set=button_set, rack_id=rack_id, slot_id=slot_id, quad=quad, modalMode=modalMode)
 
     
 
-    if request.method == 'POST':
-        if not selectedRack:
-            if quad != "true":
-                print "No valid Rack Selected"
-                flash("Please select Rack")
-                return render_template('controller-app-example.html', button=button_set, quad=quad)  
-
-        test=request.form.to_dict()
-        print "POST Data:"+str(test)
-        
-        # function for implementing sending to two racks at once
-        #ke
-        #ksTest = request.form.get('keySendTest')
-        #print ksTest
-        #if ksTest == "qwerty":
-        #    testList = {
-        #                "rack":"rack1",
-        ##                "command":"menu",
-        #                "slot":"1"
-        #                }
-                        
-        #    keySendv3(testList)
-        #    return "kstest executed"
+    
 
 
-
-        # Validation for slot id
-        if slot_id != "0":
-            print "slot id detected"
-            slotVar = str(slot_id)
-            print "slotVar:"+str(slotVar)
-        else:
-            print "no slot id detected, sending command to all stbs in rack"
-            slotVar = "1-16"
-        var1 = test.get('name', '')
-        print "var1:"
-        print var1
-        alphaVar = test.get('name2', '')
-        
-        keyword = test.get('keyword', '')
-        print keyword
-        #------
-        print "quad mode:" + str(quad)
-      
-        # check for keyword flag
-        if keyword:
-            print keyword
-            check=""
-            for letter in keyword:
-                    print "letter:"+letter
-                    digitVer = t9_trans.get(letter)
-                    print "digitVer:"+digitVer
-                    testArray = []
-                    for char in digitVer:
-                        print "Char:" + char
-                        testArray.append(char)
-                        #keySendv2(selectedRack, k, slotVar)
-                    print testArray
-                    print "char after loop:" +char
-                    if check == char:
-                        print "comparision passed!"
-                        #time.sleep(5)
-                        keySendv2(selectedRack, "rightArrow", slotVar)
-                    # sENd ir command
-                    for testArrayItem in testArray:
-                        print "testArrayItem:"+testArrayItem
-                        keySendv2(selectedRack, testArrayItem, slotVar)
-                    print "last number command sent:"+testArrayItem
-                    check = testArrayItem
-            pass
-        
-
-        if var1:
-            print 'detected value in var1'
-            if var1.isnumeric():
-                print "numeric command detected, iterating through numbers before sending commands"
-                print "quad mode:" + str(quad)
-                if quad == 'true':
-                    print rack_macs["3"]
-                    for c in var1:
-                        keySendv2(rack_macs["3"], c, '1,2,9,8')
-                        keySendv2(rack_macs["2"], c, '1,2,3,4,5,6,9,10,11,14,15,16')
-                    return render_template('controller-app-example.html', button_set=button_set, quad=quad, rack_id=rack_id, slot_id=slot_id)
-                else:
-                    for c in var1:
-                        keySendv2(selectedRack, c, slotVar)
-                    return render_template('controller-app-example.html', button_set=button_set, quad=quad, rack_id=rack_id, slot_id=slot_id)
-            else:
-                print "command string detected, sending command directly"
-                if quad == 'true':
-                    keySendv2(rack_macs["3"], var1, '1,2,9,8')
-                    keySendv2(rack_macs["2"], var1, '1,2,3,4,5,6,9,10,11,14,15,16')
-                    #return render_template('controller-app-example.html')
-                else:
-                    print "slot var:"
-                    print slotVar
-                    keySendv2(selectedRack, var1, slotVar)
-        elif alphaVar:
-            print 'name2 contents: '+ alphaVar
-          
-            if alphaVar in t9_trans:
-                print 'valid letter input found, translating to t9'
-                for i in t9_trans.get(alphaVar):
-                    keySendv2(selectedRack, i, slotVar)
-                return render_template('controller-app-example.html', button_set=button_set, quad=quad, rack_id=rack_id, slot_id=slot_id) 
-            else:
-                message = 'invalid input detected, command was not sent'
-                print message
-                return render_template('controller-app-example.html', button_set=button_set, quad=quad, rack_id=rack_id, slot_id=slot_id, error=message)    
-        else:
-            message = 'Error with Post Data Input'
-            print 'Error with Post Data Input'
-            return render_template('controller-app-example.html', button_set=button_set, quad=quad, rack_id=rack_id, slot_id=slot_id, error=message)
-        return render_template('controller-app-example.html', button_set=button_set, quad=quad, rack_id=rack_id, slot_id=slot_id)
-    else:
-        print "request was not POST"
-        return render_template('controller-app-example.html', button_set=button_set, quad=quad, rack_id=rack_id, slot_id=slot_id)
 @app.route('/main_api')
 def main_api(button_set=0, quad=0, rack_id=0):
 	return "main_api executed"
